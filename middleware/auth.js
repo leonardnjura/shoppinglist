3736a03779d68cd28b759c0d4c9e82fbe0fe16fd
Leo <leonard.njura@gmail.com>
2019-07-09 16:27:19 +0300

    backend jwt authentication

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+  const token = req.header('Authorization');
+
+  //check for token
+  if (!token) res.status(401).json({ msg: 'No token provided' });
+
+  try {
+    // verify token
+    const decoded = jwt.verify(token, config.get('JWT_SECRET'));
+
+    // add user from payload
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ msg: 'Invalid token' });
+  }
+}
+
+module.exports = auth;
--- server.js
+++ server.js
@@ -1,32 +1,39 @@
+require('dotenv').config();         // to access .env private secrets n stuff
+const config = require('config');   // to access /config/default.json public secrets n stuff
+
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
-
-const items = require('./routes/api/items');
 
 const app = express();
 
 // middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
-// db config
-const db = require('./config/keys').mongoURI;
+const { NODE_ENV } = process.env
+const isDevMode = NODE_ENV === 'development'
+const isProductionMode = NODE_ENV === 'production'
+
+// config
+const quote = isDevMode ? config.get('TAFAKARI_LA_BABU') : config.get('QUOTE_OF_THE_DAY')
+const db = process.env.MONGO_URI;
 
 //connect to mongo
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log('MongoDb connected..'))
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log('MongoDb connected...'))
   .catch(err => console.log(err));
 
 // use routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // or use..
 
 // serve static assets if production
 // remember to insert post build script, :)
-if (process.env.NODE_ENV === 'production') {
+if (isProductionMode) {
   // set static folder
   app.use(express.static('client/build'));
 
@@ -36,6 +43,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(PORT, () => console.log(`${quote}, :) \nServer started on port ${PORT}`));
